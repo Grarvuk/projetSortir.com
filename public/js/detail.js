b85b05d1c523c59c59fcb0eaf5027ab4364f67dd
Grarvuk <grarvuk@gmail.com>
2020-10-15 09:31:55 +0200

    Correction texte flash message inscription

--- public/js/detail.js
+++ public/js/detail.js
@@ -134,13 +134,13 @@ $( document ).ready(function() {
                 if(etatRequete=="success")
                 {
                     $("#flashMessage").append("<div class='alert-success'>Vous êtes inscrit.</div>");
-                    $(".svgRegister").removeClass( "svgInscri" ).addClass( "svgNonInscri" );
+                    $(".svgRegister").removeClass( "svgNonInscri" ).addClass( "svgInscri" );
             
                     $(".svgRegister").find("svg").remove();
                     $(".svgRegister").find('#txtBtnRegister').remove();
-                    $(".svgRegister").append('<svg class="svgPerso" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 19h-4v-2h4v2zm2.946-4.036l3.107 3.105-4.112.931 1.005-4.036zm12.054-5.839l-7.898 7.996-3.202-3.202 7.898-7.995 3.202 3.201zm-6 8.92v3.955h-16v-20h7.362c4.156 0 2.638 6 2.638 6s2.313-.635 4.067-.133l1.952-1.976c-2.214-2.807-5.762-5.891-7.83-5.891h-10.189v24h20v-7.98l-2 2.025z"/></svg>')
-                    $(".svgRegister").append('<span id="txtBtnRegister"> S\'inscrire</span>');
-                    
+                    $(".svgRegister").append("<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z'/></svg>")
+                    $(".svgRegister").append('<span id="txtBtnRegister"> Vous êtes inscrit !</span>');
+            
                     $(".svgRegister").trigger('svgInscri');
             
                     updateTableParticipant(idSorti);
